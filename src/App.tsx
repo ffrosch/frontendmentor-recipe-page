f0ba2b2f6c1ca8c96603793c867b7ff3a979f57e
Florian Frosch <22937059+ffrosch@users.noreply.github.com>
2024-05-29 17:42:28 +0200

    feat: set up Recipe component with interface and mock data

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import { useEffect, useState } from 'react';
 import * as styles from './App.css.ts';
+import Recipe, { IRecipe } from './components/Recipe.tsx';
 import './cssReset.css';
 import './styles.css.ts';
 import './theme.css.ts';
 
 const App = () => {
+  const [recipe, setRecipe] = useState<IRecipe | null>(null);
+
+  useEffect(() => {
+    async function getData() {
+      const data: IRecipe = await import('./../data/mockRecipe.json');
+      setTimeout(() => {
+        setRecipe(data);
+      }, 1000);
+    }
+
+    getData();
+  }, []);
+
   return (
     <main className={styles.main}>
-      <h1>Hello World!</h1>
+      {recipe ? <Recipe {...recipe} /> : 'Loading...'}
     </main>
   );
 };
---
+++ src/components/Recipe.tsx
@@ -0,0 +1,40 @@
+export interface IRecipe {
+  title: string;
+  description: string;
+  time: {
+    preparation: number;
+    cooking: number;
+  };
+  ingredients: string[];
+  instructions: string[];
+  nutrition: {
+    calories: number;
+    carbs: number;
+    protein: number;
+    fat: number;
+  };
+}
+
+export default function Recipe(props: IRecipe) {
+  const { title, description, time, ingredients, instructions, nutrition } =
+    props;
+
+  return (
+    <article>
+      <section>
+        <h1>{title}</h1>
+        <h2>Summary</h2>
+        <h3>Preparation time</h3>
+      </section>
+      <section>
+        <h2>Ingredients</h2>
+      </section>
+      <section>
+        <h2>Instructions</h2>
+      </section>
+      <section>
+        <h2>Nutrition</h2>
+      </section>
+    </article>
+  );
+}
