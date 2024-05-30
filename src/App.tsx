6f84c175e60968a6111b7b1b727760d95f787832
Florian Frosch <22937059+ffrosch@users.noreply.github.com>
2024-05-30 12:44:49 +0200

    fix: reduce simulated loading time dramatically

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import { useEffect, useState } from 'react';
 import * as styles from './App.css.ts';
 import Recipe, { IRecipe } from './components/Recipe.tsx';
-import './cssReset.css';
-import './styles.css.ts';
-import './theme.css.ts';
 
 const App = () => {
   const [recipe, setRecipe] = useState<IRecipe | null>(null);
@@ -13,7 +10,7 @@ const App = () => {
       const data: IRecipe = await import('./../data/mockRecipe.json');
       setTimeout(() => {
         setRecipe(data);
-      }, 1000);
+      }, 50);
     }
 
     getData();
