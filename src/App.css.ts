6541ef0124be89e9171501352e426ccbbd88ea00
Florian Frosch <22937059+ffrosch@users.noreply.github.com>
2024-05-30 17:55:59 +0200

    fix: need height 100% instead of 100dvh for bottom margin to take effect

--- src/App.css.ts
+++ src/App.css.ts
@@ -3,6 +3,6 @@ import { style } from '@vanilla-extract/css';
 export const main = style({
   display: 'grid',
   placeItems: 'center',
-  height: '100dvh',
+  height: '100%',
   width: '100vw',
 });
